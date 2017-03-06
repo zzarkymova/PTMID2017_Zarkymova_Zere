f7eb08aa05663b430882d428bbe4aabc8dfb4a03
zzarkymova <zarkymova@mail.ru>
2017-03-06 16:42:08 +0600

    Add project files.

---
+++ midterm_task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midterm_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr = Console.ReadLine();
+            string[] arrr = arr.Split(' ');
+            int[] myInts = Array.ConvertAll(arrr, int.Parse);
+            for (int i = 0; i < myInts.Length; ++i)
+            {
+                if (myInts [i]%1000 == 0 && myInts [i] % 15 != 0)
+                {
+                    int num = myInts[i] / 1000;
+                    if (num%10 != 0)
+                    {
+                        Console.WriteLine(myInts[i]);
+                        Console.WriteLine(' ');
+                    } 
+                    
+                   
+                }
+                
+            }
+                 
+            Console.ReadKey();
+
+        }
+    }
+}
